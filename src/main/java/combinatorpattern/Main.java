16485b158c8efbb90921764248499c5d350bf09b
Amol.Sarwade <amol.sarwade@digitaleconomysolutions>
2020-02-13 12:44:59 +0400

    Combinator patter code example

--- src/main/java/combinatorpattern/Main.java
+++ src/main/java/combinatorpattern/Main.java
@@ -2,17 +2,36 @@ package combinatorpattern;
 
 import java.time.LocalDate;
 
+import static combinatorpattern.CustomerRegistrationValidator.*;
+
 public class Main {
 
     public static void main(String[] args) {
         Customer customer = new Customer(
                 "Alice",
                 "alice@gmail.com",
-                "+0843523234234",
+                "0843523234234",
                 LocalDate.of(1991, 8, 20));
 
-        CustomerValidatorService validatorService = new CustomerValidatorService();
-        boolean valid = validatorService.isValid(customer);
-        System.out.println(valid);
+        //CustomerValidatorService validatorService = new CustomerValidatorService();
+        //boolean valid = validatorService.isValid(customer);
+        //System.out.println(valid);
+
+        //Using combinator patter
+        ValidationResult result = isEmailValid()
+                .and(isPhoneValid())
+                .and(isAdult())
+                .apply(customer);
+
+        System.out.println(result);
+
+        if (result != ValidationResult.SUCCESS) {
+            throw new IllegalStateException(result.name());
+        }
+
+        //customerRegistrationValidator is lazy nothing executes
+        CustomerRegistrationValidator customerRegistrationValidator = isEmailValid()
+                .and(isPhoneValid())
+                .and(isAdult());
     }
 }
